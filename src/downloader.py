1afbe49b00c6b2fa08adae183e155accce9b6658
legendword <zwh@legendword.com>
2025-04-19 20:25:25 -0700

    Update downloader to use unsigned requests

--- src/downloader.py
+++ src/downloader.py
@@ -1,14 +1,21 @@
-"""This module gets the San Francisco bus data from the Bus Observatory: 
+"""This module gets the San Francisco bus data from the Bus Observatory:
 https://api.busobservatory.org/"""
 
 import os
 import boto3
 from constants import DATA_DIR
+from botocore import UNSIGNED
+from botocore.config import Config
+
+# Use unsigned requests to avoid setting aws credentials
+config = Config(
+    signature_version=UNSIGNED,
+)
 
 
 def fetch(bucket, key, target):
     """Fetches a single file"""
-    fname = key.split('/')[-1].split("_")[3] + ".parquet"
+    fname = key.split("/")[-1].split("_")[3] + ".parquet"
     path = os.path.join(target, fname)
     os.makedirs(target, exist_ok=True)
     bucket.download_file(key, path)
@@ -16,7 +23,7 @@ def fetch(bucket, key, target):
 
 def download_bus_data():
     """Downloads San Francisco bus data"""
-    s3 = boto3.resource("s3")
+    s3 = boto3.resource("s3", config=config)
     bucket = s3.Bucket("busobservatory-lake")
     objects = bucket.objects.filter(Prefix="feeds/sf_muni/COMPACTED")
     bus_data_dir = os.path.join(DATA_DIR, "bus", "sf")
