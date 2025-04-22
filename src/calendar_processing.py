5aeebe58159f110355d250439d8b60810b982f87
Linus Moreau <linus.moreau@gmail.com>
2025-04-21 17:31:08 -0700

    Add calendar data

---
+++ src/calendar_processing.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import datetime
+import holidays
+
+US_HOLIDAYS = holidays.US()
+
+def build_calendar(start="2023-01-01", end="2025-04-14") -> pd.DataFrame:
+    start = datetime.datetime.strptime(start, "%Y-%m-%d").date()
+    end = datetime.datetime.strptime(end, "%Y-%m-%d").date()
+    date = start
+    calendar = []
+    while date <= end:
+        day_of_week = date.strftime("%A")
+        calendar.append([
+            date,
+            int(date in US_HOLIDAYS),
+            int(day_of_week == "Monday"),
+            int(day_of_week == "Tuesday"),
+            int(day_of_week == "Wednesday"),
+            int(day_of_week == "Thursday"),
+            int(day_of_week == "Friday"),
+            int(day_of_week == "Saturday"),
+            int(day_of_week == "Sunday"),
+        ])
+        date += datetime.timedelta(1)
+    return pd.DataFrame(calendar, columns=[
+        "date",
+        "holiday",
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+        "sunday"
+    ]).set_index("date")
